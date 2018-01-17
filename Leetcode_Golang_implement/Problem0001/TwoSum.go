c2b5fa454ad089ef961fbd309e0a149bec32f0b3
zhangwenqiang <zhangwenqiang_>
2018-01-17 15:24:25 +0800

    TestLeetcode@Office

--- Leetcode_Golang_implement/Problem0001/TwoSum.go
+++ Leetcode_Golang_implement/Problem0001/TwoSum.go
@@ -1,1 +1,13 @@
 package Problem0001
+
+func twoSum(nums[] int,target int) []int{
+	m := make(map[int]int,len(nums))
+
+	for i,b:=range nums {
+		if j,ok := m[target-b];ok {
+			return []int{i,j}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
