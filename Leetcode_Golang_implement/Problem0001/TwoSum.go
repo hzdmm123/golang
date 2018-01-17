609226f55cebe221c9fe5143d82441d6ec272951
zhangwenqiang <zhangwenqiang_>
2018-01-17 15:22:45 +0800

    TestGoCheck

---
+++ Leetcode_Golang_implement/Problem0001/TwoSum.go
@@ -0,0 +1,1 @@
+package Problem0001
--- TestGoCheck/TestGoCheck_test.go
+++ TestGoCheck/TestGoCheck_test.go
@@ -2,8 +2,9 @@ package hello_test
 
 import (
 	"testing"
-	"io"
+	//"io"
 	. "gopkg.in/check.v1"
+	//"fmt"
 )
 
 // Hook up gocheck into the "go test" runner.
@@ -13,6 +14,7 @@ type MySuite struct{}
 
 var _ = Suite(&MySuite{})
 
+/*
 func (s *MySuite) TestHelloWorld(c *C) {
 	c.Assert("42", Equals, "42")
 	c.Assert(io.ErrClosedPipe, ErrorMatches, "io: .*on closed pipe")
@@ -26,4 +28,19 @@ func (s *MySuite) SetUpTest(c *C){
 
 func (s * MySuite) TearDownTest(c *C)  {
 	c.Log("after test do this tearDown test")
+}
+*/
+
+
+func (s *MySuite) BenchmarkLogicTest1(c *C) {
+	for i := 0; i < c.N; i++ {
+		// Logic to benchmark
+	}
+}
+
+func (s *MySuite) BenchmarkLogicTest(c *C) {
+	for i := 0; i < c.N; i++ {
+		// Logic to benchmark
+
+	}
 }
